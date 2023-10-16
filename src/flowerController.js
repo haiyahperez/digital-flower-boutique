0257a30b155c3c4043c1da1a2dba45becb9e978a
Haiyah Perez <haiyahperez@Haiyahs-Air.fios-router.home>
2023-10-16 12:24:55 -0400

    index function

--- src/flowerController.js
+++ src/flowerController.js
@@ -16,43 +16,14 @@ function create(flowers, flowerName) {
   flowers.push(flower)
   return flower;
 }
-
 function index(flowers) {
   return flowers.map((flower) => flower.id + " " + flower.name).join("\n");
 }
 
-function show(flower, flowerId) {
-  const flower = flower.find((flower) => flower.id === flowerId);
-  return flower.name + " " + flower.id;
-}
-function destroy(flowers, flowerId) {
-  const index = flowers.findIndex((flower) => flower.id === flowerId);
-  if (index > -1) {
-    flowers.splice(index, 1);
-    inform('Flower successfully removed from collection');
-    return flowers;
-  } else {
-    inform('Flower not found. No action taken');
-    return flowers;
-  }
-}
-function update(flower, flowerId, updatedFlower) {
-  const index = flower.findIndex((flower) => flower.id === flowerId);
-  if (index > -1) {
-    flower[index].id = flowerId;
-    flower[index].name = updatedFlower;
-    inform('Flower successfully updated');
-    return flower;
-  } else {
-    inform('Flower not found. No action taken');
-    return flower;
-  }
-}
-
 module.exports = {
   create,
-  index,
-  show,
-  destroy, 
-  update
+  index
+//   show,
+//   destroy, 
+//   update
 };
